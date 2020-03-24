d98e49bc38b0f2e096a3a87b07e2f2e95be4bb55
bertearazvan <bertea74@gmail.com>
2020-03-24 12:46:44 +0100

    dns to build

---
+++ build/precache-manifest.3deade3ed0bf9cb32dd7b075ae4f51e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a464b01ed8976aaadbcd13bc48eb12e",
+    "url": "/other/react-weather-app/index.html"
+  },
+  {
+    "revision": "94beb47c5d555e926451",
+    "url": "/other/react-weather-app/static/css/main.69e26e86.chunk.css"
+  },
+  {
+    "revision": "da6f394e89afd67b2398",
+    "url": "/other/react-weather-app/static/js/2.addf5a64.chunk.js"
+  },
+  {
+    "revision": "cf2ee3023b1592f4f0a84068fb0cb0c2",
+    "url": "/other/react-weather-app/static/js/2.addf5a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94beb47c5d555e926451",
+    "url": "/other/react-weather-app/static/js/main.31fe31cf.chunk.js"
+  },
+  {
+    "revision": "2e4574a2332147e27a17",
+    "url": "/other/react-weather-app/static/js/runtime-main.b71d8839.js"
+  }
+]);
